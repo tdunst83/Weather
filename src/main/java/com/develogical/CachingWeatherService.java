d238c5876c7e9bfea51248a1226b220bf425c23e
tdunst83 <thomas.dunst@avalanchez.de>
2019-03-20 17:39:19 +0000

    removing the first entry

--- src/main/java/com/develogical/CachingWeatherService.java
+++ src/main/java/com/develogical/CachingWeatherService.java
@@ -4,6 +4,7 @@ import com.weather.Day;
 import com.weather.Region;
 
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
@@ -13,7 +14,7 @@ public class CachingWeatherService implements WeatherService {
 
     private Map<String,String> cityDayCache = new LinkedHashMap<String,String>();
 
-    private int numberOfCachedEntries = 3;
+    private int numberOfCachedEntries = 2;
 
 
     public CachingWeatherService(WeatherService delegate) {
@@ -28,6 +29,21 @@ public class CachingWeatherService implements WeatherService {
         if (!cityDayCache.containsKey(mapKey)) {
             String weather = weatherService.getWeather(region, day);
 
+
+            if(cityDayCache.size()>=numberOfCachedEntries){
+
+                String mapKey2 = "";
+
+                Iterator<Map.Entry<String,String>> entries = cityDayCache.entrySet().iterator();
+
+                if (entries.hasNext()) {
+                    Map.Entry<String, String> entry = entries.next();
+                    mapKey2 = entry.getKey();
+                }
+
+                cityDayCache.remove(mapKey2);
+
+            }
 
             cityDayCache.put(mapKey, weather);
         }
