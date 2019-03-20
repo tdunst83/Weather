2812d94bd114668f29ffec5a26e8fd89f8751fe8
Steven Taylor <taylorsj1980@gmail.com>
2019-03-20 16:54:54 +0000

    Refactor version of maps

--- src/main/java/com/develogical/CachingWeatherService.java
+++ src/main/java/com/develogical/CachingWeatherService.java
@@ -10,7 +10,7 @@ public class CachingWeatherService implements WeatherService {
 
     private WeatherService weatherService;
 
-    private Map<Region,Map<Day,String>> cityDayCache = new HashMap<Region,Map<Day,String>>();
+    private Map<String,String> cityDayCache = new HashMap<String,String>();
 
     public CachingWeatherService(WeatherService delegate) {
         weatherService = delegate;
@@ -19,22 +19,15 @@ public class CachingWeatherService implements WeatherService {
     @Override
     public String getWeather(Region region, Day day) {
 
-        if (!cityDayCache.containsKey(region) || !cityDayCache.get(region).containsKey(day) ) {
+        String mapKey = region.toString() + day.toString();
+
+        if (!cityDayCache.containsKey(mapKey)) {
             String weather = weatherService.getWeather(region, day);
 
-            Map<Day,String> helpMap;
-            if (cityDayCache.containsKey(region)){
-                helpMap = cityDayCache.get(region);
-            }else{
-                helpMap = new HashMap<Day,String>();
-            }
-
-            helpMap.put(day,weather);
-
-            cityDayCache.put(region,helpMap);
+            cityDayCache.put(mapKey, weather);
         }
 
-        return cityDayCache.get(region).get(day);
+        return cityDayCache.get(mapKey);
     }
 
 }
